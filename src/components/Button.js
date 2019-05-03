bc00bdcef398bd6da0c878912f305313dd2bc6ff
Astramentis <WesleyFLVS@gmail.com>
2019-05-03 19:58:24 -0400

    Added buttons and rows
    definied button in css/js

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+
+class Button extends Component {
+  render() {
+      return(
+          <div className="App">
+          </div>
+      );}
+  }
+export default Button;
