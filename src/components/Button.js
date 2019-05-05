65ba22585156d72dda0a98de02202d1de2077ff1
Astramentis <WesleyFLVS@gmail.com>
2019-05-04 23:48:33 -0400

    Created function to detect operators

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import './Button.css';
 
 class Button extends Component {
+    isOperator = val => {
+        return !isNaN(val) || val === "." || val === "=";
+    };
   render() {
       return (
-          <div className="button">
-          {this.props.children}
-          </div>
-      );}
+        <div 
+          className={`button ${this.isOperator
+            (this.props.children)?"":"operator"}`}
+          >
+            {this.props.children}
+        </div>
+      );};
   }
 export default Button;
