35aea0ca9d7ae13652f5b861baa7d580be87cc0b
Astramentis <WesleyFLVS@gmail.com>
2019-05-07 19:43:06 -0400

    Added input
    no logic, area for numbers to be entered

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Input.css';
+
+class Input extends Component {
+  render() {
+      return (
+        <div className="input"> 
+         sadf
+        </div>
+      );};
+  }
+export default Input;
